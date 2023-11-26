de671f764f8eb4c9eca380871ab30d37f7a3a682
Vince Vahala <vincevahala@gmail.com>
2023-11-26 11:22:29 -0700

    fixed weathercamcontainer.js and cleanup

--- src/components/WeatherCamContainer.js
+++ src/components/WeatherCamContainer.js
@@ -11,7 +11,8 @@ import Typography from '@material-ui/core/Typography';
 
 const styles = theme => ({
   card: {
-    // maxWidth: 350,
+    width: '100%',
+    height: '100%',
   },
   media: {
     textAlign: 'center',
@@ -29,136 +30,51 @@ function WeatherCamContainer(props) {
   return (
     <Fragment>
       <h2>Colorado Web Cams</h2>
-      <Grid container spacing={16}>
-        <Grid item xs={12} md={4}>
+      <Grid container spacing={2}>
+        <Grid item xs={12} md={6}>
           <Card className={classes.card}>
             <CardHeader
               className="card-header"
               avatar={
-                <Avatar aria-label="Recipe" className={classes.avatar}>
-                  D
+                <Avatar className={classes.avatar}>
+                  B
                 </Avatar>
               }
-              title="Denver Live Weather Cam"
+              title="Boulder Live Weather Cam"
             />
           <CardMedia className={classes.media} src={classes.source}>
-              <iframe title="Denver City Live Cam" src="https://www.youtube-nocookie.com/embed/Fq68_K0YlIA?autoplay=1" frameBorder="0" width="auto" height="100%" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+            <iframe width="100%" height="315" src="https://www.youtube-nocookie.com/embed/G2b6B8ZExmo?si=tZAVvfJcmulQzymN" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
           </CardMedia>
             <CardContent>
               <Typography component="p">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua
+                Live views from Pearl Street in Boulder.  The flatirons, flagstaff, Sanitas, West Pearl street view and finally the view to the east.
               </Typography>
             </CardContent>
           </Card>
         </Grid>
 
-        <Grid item xs={12} md={4}>
+        <Grid item xs={12} md={6}>
           <Card className={classes.card}>
             <CardHeader
               avatar={
-                <Avatar aria-label="Recipe" className={classes.avatar}>
-                  G
+                <Avatar className={classes.avatar}>
+                  F
                 </Avatar>
               }
               title="Boulder Flatirons Cam"
             />
           <CardMedia className={classes.media} src={classes.source}>
-            <iframe title="Boulder Flatirons Cam" width="auto" height="100%" src="https://lookr.com/1193938385" frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+          <iframe width="100%" height="315" src="https://www.youtube-nocookie.com/embed/QXHEQqEbec0?si=vE5TeNUUM06UTA72" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
           </CardMedia>
             <CardContent>
               <Typography component="p">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua
-              </Typography>
-            </CardContent>
-          </Card>
-        </Grid>
-
-        <Grid item xs={12} md={4}>
-          <Card className={classes.card}>
-            <CardHeader
-              avatar={
-                <Avatar aria-label="Recipe" className={classes.avatar}>
-                  B
-                </Avatar>
-              }
-              title="Breckenridge Mountain Cam"
-            />
-          <CardMedia className={classes.media} src={classes.source}>
-              <iframe title="Breckenridge Mountain Cam" src="https://www.youtube-nocookie.com/embed/pM7R7EArdzo?autoplay=1" width="auto" height="100%"  frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
-          </CardMedia>
-            <CardContent>
-              <Typography component="p">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua
-              </Typography>
-            </CardContent>
-          </Card>
-        </Grid>
-
-        <Grid item xs={12} md={4}>
-          <Card className={classes.card}>
-            <CardHeader
-              avatar={
-                <Avatar aria-label="Recipe" className={classes.avatar}>
-                  D
-                </Avatar>
-              }
-              title="Durango City Cam"
-            />
-          <CardMedia className={classes.media} src={classes.source}>
-            <iframe title="Durango City Cam" src="https://www.youtube-nocookie.com/embed/NeHa4JHfB1A?autoplay=1" width="auto" height="100%"  frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
-          </CardMedia>
-            <CardContent>
-              <Typography component="p">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua
-              </Typography>
-            </CardContent>
-          </Card>
-        </Grid>
-
-        <Grid item xs={12} md={4}>
-          <Card className={classes.card}>
-            <CardHeader
-              avatar={
-                <Avatar aria-label="Recipe" className={classes.avatar}>
-                  T
-                </Avatar>
-              }
-              title="Telluride City Cam"
-            />
-          <CardMedia className={classes.media} src={classes.source}>
-            <iframe title="Telluride City Cam" width="auto" height="100%" src="https://www.youtube-nocookie.com/embed/Fhj-oPwX1bo?autoplay=1" frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
-          </CardMedia>
-            <CardContent>
-              <Typography component="p">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua
-              </Typography>
-            </CardContent>
-          </Card>
-        </Grid>
-
-        <Grid item xs={12} md={4}>
-          <Card className={classes.card}>
-            <CardHeader
-              avatar={
-                <Avatar aria-label="Recipe" className={classes.avatar}>
-                  G
-                </Avatar>
-              }
-              title="Glenwood Springs Cam"
-            />
-          <CardMedia className={classes.media} src={classes.source}>
-            <iframe title="Glenwood Springs Cam" width="auto" height="100%" src="https://www.youtube-nocookie.com/embed/gDPnP_gJf_A?autoplay=1" frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
-          </CardMedia>
-            <CardContent>
-              <Typography component="p">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua
+                Located in Louisville, this cam provides sweeping views of the Boulder "Skyline", spanning from Eldorado Canyon to Longs Peak.
               </Typography>
             </CardContent>
           </Card>
         </Grid>
 
       </Grid>
-      <h2>Utah Web Cams</h2>
     </Fragment>
   );
 }
